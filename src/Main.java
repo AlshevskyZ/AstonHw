95fba6b0358d3399ec59c07001346c738b5e3977
AlshevskyZ <zvalshevsky@gmail.com>
2024-04-15 21:58:48 +0300

    Lesson5

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,41 @@
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        String[][] array1 = new String[][]{{"1", "2", "3", "4"},
-                                           {"5", "6", "7", "8"},
-                                           {"9", "10", "11", "12"},
-                                           {"13", "14", "15", "16"},
-                                           {"17","18","19","20"}};
-        String[][] array2 = new String[][]{{"1", "2", "3", "4"},
-                                           {"5", "6", "7", "8"},
-                                           {"9", "10", "11", "12"},
-                                           {"13", "!!!!!!!", "15", "16"}};
-        String[][] array3 = new String[][]{{"1", "2", "3", "4"},
-                                           {"5", "6", "7", "8"},
-                                           {"9", "10", "11", "12"},
-                                           {"13", "14", "15", "16"}};
-        String[][][] arrays = new  String[][][]{array1, array2, array3};
-        for(String[][] arr : arrays){
-            try {
-                System.out.println("Сумма элементов массива: " + calculateArray(arr));
-            } catch (MyArraySizeException | MyArrayDataException e) {
-                System.out.println(e.getMessage());
+        String[] months = {"Январь", "Феварль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август",
+                "Сентябрь", "Октябрь","Ноябрь","Декабрь","Январь","Феварль", "Март", "Апрель", "Май",
+                "Июнь", "Июль", "Август", "Сентябрь"};
+        Set<String> uniqueWords = new HashSet<>(List.of(months));
+        System.out.println("Уникальные слова:");
+        for (String word : uniqueWords) {
+            System.out.print(word + "\n");
+        }
+        Map<String, Integer> wordCounts = new HashMap<>();
+        for (String word : months) {
+            int count = wordCounts.getOrDefault(word, 0);
+            wordCounts.put(word, count + 1);
+        }
+        System.out.println("\nЧастота встречаемости слов:");
+        for (Map.Entry<String, Integer> entry : wordCounts.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+        ////////////////////////////////////////////// Задание2
+        class PhoneBook {
+            private Map<String, List<Integer>> contacts;
+            public PhoneBook() {
+                contacts = new HashMap<>();
             }
-        }
-    }
-    public static int calculateArray(String[][] array) throws MyArraySizeException,MyArrayDataException {
-        if (array.length != 4 || array[0].length != 4) {
-            throw new MyArraySizeException();
-        }
-        int sum=0;
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                try {
-                    sum += Integer.parseInt(array[i][j]);
-                } catch (NumberFormatException e) {
-                    throw new MyArrayDataException(i, j);
+            public void add(String surname, int phoneNumber) {
+                if (contacts.containsKey(surname)) {
+                    contacts.get(surname).add(phoneNumber);
+                } else {
+                    List<Integer> numbers = new ArrayList<>();
+                    numbers.add(phoneNumber);
+                    contacts.put(surname, numbers);
                 }
             }
+            public List<Integer> get(String surname) {
+                return contacts.get(surname);
+            }
         }
-        return sum;
     }
 }
